6f5ed6b332dc6d7b5298c1362cf63a89c84ee5d1
mmrtnz <muhammad_martinez@yahoo.com>
2021-05-13 11:01:44 -0500

    Adds new line to webpack config and fixes server issue finding static files

--- examples/server.js
+++ examples/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 var path = require("path");
 const app = express();
 
-app.get('*', (req, res) => {
-  // You can replace this file with any example html
+app.use('/src', express.static('src'));
+
+app.get('/', (req, res) => {
+  // You can replace this file with any example html file
   res.sendFile(path.join(__dirname + '/vanilla_css_styles.html'));
 });
 
